be5c15d64c46f8fc9f3717a69a8acaecee418417
Mrxuccc <72081522+Mrxuccc@users.noreply.github.com>
2021-08-10 19:33:54 +0800

    Add files via upload

---
+++ store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+const store = new 
+Vuex.Store({
+	state: { //存放状态
+	"username": "fff",
+	"password": '123456',
+	"authentication":true
+	}
+})
+ 
+export default store;

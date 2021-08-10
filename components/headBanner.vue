aa9a2ae21515ff7bec1c3d2d8acc3760421bd5c3
Mrxuccc <72081522+Mrxuccc@users.noreply.github.com>
2021-08-10 19:32:55 +0800

    Add files via upload

---
+++ components/headBanner.vue
@@ -0,0 +1,51 @@
+<template>
+	<view>
+		<view class="headBanner">
+			<view class="circular">
+				<view class="circularCenter">
+					企业入驻
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"headBanner",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+	.headBanner {
+		width: 100%;
+		height:265rpx;
+		background-color: #74d69b;
+	}
+	
+	.circular {
+		margin: 24rpx 0 0 310rpx;
+		position: absolute;
+		width: 140rpx;
+		height: 140rpx;
+		background-color: #FFFFFF;
+		border-radius: 70rpx;
+	}
+	
+	.circularCenter {
+		position: relative;
+		top: 24rpx;
+		left: 38rpx;
+		width: 70rpx;
+		height: 70rpx;
+		font-size: 14px;
+		font-weight: 700;
+		color: #63B2FF;
+	
+	}
+</style>
